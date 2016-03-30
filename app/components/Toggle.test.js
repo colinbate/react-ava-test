657e1e0ce38c4efbd79dbad0cdaf0e757912924d
Colin Bate <colin@colinbate.com>
2016-03-30 10:00:10 -0300

    Add tests

--- app/components/Toggle.test.js
+++ app/components/Toggle.test.js
@@ -1,4 +1,21 @@
 import test from 'ava'
+import sinon from 'sinon' // you should have installed this in the last step
 
-test('empty test', t => t.pass())
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
 
+import Toggle from './Toggle'
+
+test('toggle--off class applied by default', t => {
+  const renderer = TestUtils.createRenderer();
+  renderer.render(<Toggle/>);
+  const result = renderer.getRenderOutput().props.className.includes('toggle--off');
+  t.true(result);
+})
+
+test('toggle--on class applied when initialToggledOn specified to true', t => {
+  const renderer = TestUtils.createRenderer();
+  renderer.render(<Toggle initialToggledOn={true}/>);
+  const result = renderer.getRenderOutput().props.className.includes('toggle--on');
+  t.true(result);
+})
--- app/store/Customers.test.js
+++ app/store/Customers.test.js
@@ -1,5 +1,31 @@
 import test from 'ava'
+import sinon from 'sinon' // you'll need to install this with `npm install --save-dev sinon`
+import store from './Customers'
 
-test('empty test', t => t.pass())
+test('customers should start with empty', t => {
+  // call store.getCustomers and verify the result is empty
+  const custs = store.getCustomers();
+  t.is(custs.length, 0);
+})
 
+test('setting customers and getting them', t => {
+  const testval1 = { name: 'Colin' };
+  const testval2 = { name: 'Bate' };
+  store.setCustomers([testval1, testval2]);
+  const custs = store.getCustomers();
+  t.is(custs.length, 2);
+  t.is(custs[0].name, 'Colin');
+})
 
+test('subscribing to the store', t => {
+  const spy = sinon.spy();
+  const unsub = store.subscribe(spy);
+  store.setCustomers([{name: 'Test'}]);
+  t.true(spy.calledOnce);
+  spy.reset();
+  unsub();
+  store.setCustomers([{name: 'Test2'}]);
+  t.false(spy.calledOnce);
+})
+
+// add an afterEach here to reset the customers to an empty array
